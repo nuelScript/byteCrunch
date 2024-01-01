f19ffa24e4cda84260a9f238670c7d6e9774c43f
Parseval <igwee3333@gmail.com>
2024-01-01 03:55:58 +0100

    chore: added register form and server and server action

---
+++ actions/register.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import * as z from "zod";
+
+import { RegisterSchema } from "@/schemas";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields) {
+    return { error: "Invalid Fields!" };
+  }
+
+  return { success: "Email sent!" };
+};
--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,12 @@ export const LoginSchema = z.object({
   email: z.string().email({ message: "Email is required" }),
   password: z.string().min(1, { message: "Password is required" }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+  password: z.string().min(8, { message: "Minimum of 8 characters required." }),
+  name: z.string().min(1, { message: "Name is required" }),
+  matricNo: z
+    .string()
+    .min(10, { message: "Minimum of 10 characters required." }),
+});
