d784909f2209ecc19a8b40e4fa9dac9f6757f8c5
Parseval <igwee3333@gmail.com>
2024-01-02 00:47:30 +0100

    chore: hash added

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,41 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return { error: "Invalid Fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name, matricNo } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { errror: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+      matricNo,
+    },
+  });
+
+  // TODO: Send email verification
+
+  return { success: "User created!" };
 };
