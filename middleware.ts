deb24d197203741c5632e6b91e1ca41e66fcc63a
Parseval <igwee3333@gmail.com>
2024-01-03 02:11:32 +0100

    feat: updated next version

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,5 @@ export default auth((req) => {
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)"],
 };
