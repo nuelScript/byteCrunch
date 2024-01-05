c90b2ef3a258fa0719a72662c55e6b6ec259eb31
Parseval <igwee3333@gmail.com>
2024-01-05 10:40:27 +0100

    feat: added user roles

---
+++ app/(routes)/settings/page.tsx
@@ -0,0 +1,7 @@
+import { SignOutButton } from "@/components/auth/sign-out";
+
+const SettingsPage = () => {
+  return <SignOutButton />;
+};
+
+export default SettingsPage;
--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
+import { UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
 
 export const {
   handlers: { GET, POST },
@@ -11,8 +13,25 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
     async jwt({ token }) {
-      console.log({ token });
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
