84e48f7c20d90f2ddec51f371ee6fd78118aed73
Parseval <igwee3333@gmail.com>
2024-01-12 06:48:19 +0100

    chore: changed admin route and added navbar to routes

--- app/(routes)/home/layout.tsx
+++ app/(routes)/home/layout.tsx
@@ -1,9 +1,14 @@
-import { SignOutButton } from "@/components/auth/sign-out";
+import { Footer } from "@/components/footer";
 
 export default async function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="w-full h-full">{children}</div>;
+  return (
+    <div className="w-full h-full">
+      {children}
+      <Footer />
+    </div>
+  );
 }
---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components/navbar";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default RootLayout;
